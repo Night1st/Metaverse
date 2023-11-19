502e66fced165d7146959731af2756ae897ae8c1
Night1st <ndm1011vn@gmail.com>
2023-11-19 19:15:03 +0700

    add build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
+  distDir: "build", //this line will tell the build to create a file with this name
   reactStrictMode: true,
   i18n,
   images: {
