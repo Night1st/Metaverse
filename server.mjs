88607c12dba249abc6af0e7f98763bebfbf1ba8c
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2023-12-05 23:12:08 +0700

    thuanvv/update-file-config_server

---
+++ server.mjs
@@ -0,0 +1,39 @@
+import express from 'express';
+import https from 'https';
+import fs from 'fs';
+import next from 'next';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const httpPort = 3001;
+const httpsPort = 443; // Port for HTTPS
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Define the paths you want to handle with Next.js
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  // Read the SSL certificate and key files
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/chopconfirm.sg/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/chopconfirm.sg/fullchain.pem'),
+  };
+
+  https.createServer(httpsOptions, server).listen(httpsPort, () => {
+    console.log(`HTTPS server is running on port ${httpsPort}`);
+  }).on('error', (err) => {
+    if (err) throw err;
+  });
+
+  server.listen(httpPort, () => {
+    console.log(`HTTP server is running on port ${httpPort}`);
+  }).on('error', (err) => {
+    if (err) throw err;
+  });
+
+});
